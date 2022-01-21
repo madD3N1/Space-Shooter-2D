3b99b2ce56ccf3ef89d4eb9031c035fb18bd1dd8
Daniil Kukhta <woodyw86rus@gmail.com>
2022-01-22 00:04:59 +0500

    Создан класс MovementController для управления. Сделано управление виртуальным стиком и клавиатурой.

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -48,6 +48,7 @@ namespace SpaceShooter
         /// </summary>
         public float TorqueControl { get; set; }
 
+        #region Unity Events
         protected override void Start()
         {
             base.Start();
@@ -55,6 +56,27 @@ namespace SpaceShooter
             m_Rigid = GetComponent<Rigidbody2D>();
             m_Rigid.mass = m_Mass;
             m_Rigid.inertia = 1;
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateRigidbody();
+        }
+
+        #endregion
+        
+        /// <summary>
+        /// Метод добавления сил кораблю для движения.
+        /// </summary>
+        private void UpdateRigidbody()
+        {
+            m_Rigid.AddForce(ThrustControl * m_Thrust *  transform.up * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddForce(-m_Rigid.velocity * (m_Thrust / m_MaxLinearVelocity) * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddTorque(TorqueControl * m_Mobility * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddTorque(-m_Rigid.angularVelocity * ( m_Mobility /m_MaxAngularVelocity) * Time.fixedDeltaTime, ForceMode2D.Force);
         }
     }
 }
