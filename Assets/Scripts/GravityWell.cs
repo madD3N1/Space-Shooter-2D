eef1d510f7bf66abf88d4541385d239717a00763
Daniil Kukhta <woodyw86rus@gmail.com>
2022-01-27 20:27:42 +0500

    Реализована гравитационная яма.
    Создан и реализован класс GravityWell. На сцену добавлена планета с гравитационной ямой. Улучшен background на сцене.

---
+++ Assets/Scripts/GravityWell.cs
@@ -0,0 +1,37 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class GravityWell : MonoBehaviour
+    {
+        [SerializeField] private float m_Force;
+        [SerializeField] private float m_Radius;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.attachedRigidbody == null) return;
+
+            Vector2 dir = transform.position - collision.transform.position;
+
+            float dist = dir.magnitude;
+
+            if(dist < m_Radius)
+            {
+                Vector2 force = dir.normalized * m_Force * (dist / m_Radius);
+                collision.attachedRigidbody.AddForce(force, ForceMode2D.Force);
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            GetComponent<CircleCollider2D>().radius = m_Radius;
+        }
+#endif
+    }
+}
